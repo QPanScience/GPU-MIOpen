8a23cd94c6f66aa2c183ea2b2e347d5827a21345
Jing Zhang <jizhan@amd.com>
2020-09-30 13:13:29 +0000

    [skip ci] clean solvers

--- src/mlo_dir_conv.cpp
+++ src/mlo_dir_conv.cpp
@@ -135,17 +135,8 @@ static auto GetImplicitGemmSolvers()
         miopen::solver::ConvHipImplicitGemmForwardV4R5Xdlops,
         miopen::solver::ConvHipImplicitGemmForwardV4R4Xdlops,
         miopen::solver::ConvHipImplicitGemmForwardV4R4Xdlops_Padded_Gemm,
-        miopen::solver::ConvHipImplicitGemmV4R4GenXdlopsFwdFp32,
-        miopen::solver::ConvHipImplicitGemmV4R4GenFwdXdlops,
         miopen::solver::ConvHipImplicitGemmBwdDataV4R1Xdlops,
-        miopen::solver::ConvHipImplicitGemmBwdDataV1R1Xdlops,
-        miopen::solver::ConvHipImplicitGemmV4R1Fwd,
-        miopen::solver::ConvHipImplicitGemmV4R4Fwd,
-        miopen::solver::ConvHipImplicitGemmBwdDataV1R1,
-        miopen::solver::ConvHipImplicitGemmBwdDataV4R1,
-        miopen::solver::ConvAsmImplicitGemmV4R1DynamicFwd_1x1,
-        miopen::solver::ConvAsmImplicitGemmV4R1DynamicFwd,
-        miopen::solver::ConvAsmImplicitGemmV4R1DynamicBwd>{};
+        miopen::solver::ConvHipImplicitGemmBwdDataV1R1Xdlops>{};
 }
 
 static auto GetWindogradSolvers()
@@ -167,12 +158,7 @@ static auto GetWindogradSolvers()
 
 static auto GetImplicitGemmWrWSolvers()
 {
-    return miopen::solver::SolverContainer<miopen::solver::ConvHipImplicitGemmWrwV4R4Xdlops,
-                                           miopen::solver::ConvHipImplicitGemmV4R4GenXdlopsWrWFp32,
-                                           miopen::solver::ConvHipImplicitGemmV4R4GenWrWXdlops,
-                                           miopen::solver::ConvHipImplicitGemmV4R1WrW,
-                                           miopen::solver::ConvHipImplicitGemmV4R4WrW,
-                                           miopen::solver::ConvAsmImplicitGemmV4R1DynamicWrw>{};
+    return miopen::solver::SolverContainer<miopen::solver::ConvHipImplicitGemmWrwV4R4Xdlops>{};
 }
 
 static auto GetWindogradWrWSolvers()
